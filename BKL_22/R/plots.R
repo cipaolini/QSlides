7b6770ae15ef938f266075609643f1151b465e08
Mariana <mariana.montes@kuleuven.be>
2022-10-19 13:00:38 +0200

    run new random forest

---
+++ BKL_22/R/plots.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(ggrepel)
+library(showtext) # for a fun font
+font_add_google("Schoolbell", "bell")
+showtext_auto()
+
+# Functions -------------------------------------------------------------------
+tsne_plot <- function(coords, seed = 7) {
+  # Scatterplot with t-sne coordinates, colored by cluster (and negative silhouette
+  # in gray). Instead of a legend the name of the medoid is indicated in its
+  # approximate position (and the same color).
+  ggplot(coords, aes(x = x, y = y, color = final_cluster)) +
+    geom_point(size = 3, alpha = 0.7) +
+    geom_label_repel(data = filter(coords, cw == medoid_label),
+                     aes(label = cw),
+                     seed = seed, size = 8) +
+    theme_void() +
+    scale_color_viridis_d(na.value = "gray", guide = "none") +
+    coord_fixed()
+}
+add_point <- function(p, lemma_data, add_x = 1, add_y = 1, curvature = -0.2,
+                      hjust = 1, vjust = -0.1, font_size = 10) {
+  p + annotate("point", x = lemma_data$x, y = lemma_data$y, shape = 1, size = 5) +
+    annotate("curve",
+             x = lemma_data$x + add_x, xend = lemma_data$x, 
+             y = lemma_data$y + add_y, yend = lemma_data$y,
+             size = 1, arrow = arrow(length = unit(0.15, "inches")),
+             curvature = curvature) +
+    annotate("text",
+             x = lemma_data$x + add_x, y = lemma_data$y + add_y,
+             label = lemma_data$cw, hjust = hjust, vjust = vjust, family = "bell", size = font_size)
+}
+
+clusters_barplot <- function(tokens, column) {
+  # Barplot with the number of tokens covered by each cluster, with different
+  # shades of gray for each variant.
+  ggplot(tokens, aes(x = fct_infreq({{ column }}), fill = Variant)) +
+    geom_bar() +
+    scale_fill_grey(start = 0.4, end = 0.6) +
+    labs(x = "Medoid", y = "N of tokens") +
+    theme_minimal() +
+    theme(legend.position = c(0.9, 0.9)) +
+    coord_flip()
+}
+varimp_dotplot <- function(varimp_data) {
+  varimp_data %>% 
+    mutate(
+      Predictor = str_remove(Predictor, ".bin"),
+      Slot = str_extract(Predictor, "^[RT][a-z]+"),
+      Slot = if_else (is.na(Slot), "Traditional", Slot)) %>% 
+    ggplot(aes(x = Varimp, y = reorder(Predictor, Varimp),
+               color = Slot)) +
+    geom_point(size = 3) +
+    scale_color_viridis_d(begin = 0.2, end = 0.8, option = "A") +
+    labs(x = "Variable importance", y = "Predictor") +
+    geom_vline(xintercept = abs(min(varimp_data$Varimp)),
+               color = "gray30", linetype = 2) +
+    theme_minimal(base_size = 20) +
+    theme(legend.position = c(0.9, 0.2))
+}
+
+## Actual plotting -------------------------------------------------------------
+# processed_data <- here::here("BKL_22", "data_processed")
+# recipient_coords <- read_tsv(file.path(processed_data, "recipient_coords.tsv"), show_col_types = FALSE)
+# tsne_plot(recipient_coords) %>%
+#   add_point(recipient_coords %>% filter(cw == "government/nn"), -6, 0) %>%
+#   add_point(recipient_coords %>% filter(cw == "mother/nn"), -7)
+# 
+# theme_coords <- read_tsv(file.path(processed_data, "theme_coords.tsv"), show_col_types = FALSE)
+# tsne_plot(theme_coords) %>%
+#   add_point(theme_coords %>% filter(cw == "it/pp"), -7, -10, vjust = 1)
+# 
+# tokens <- read_tsv(file.path(processed_data, "dat_clusters.tsv"), show_col_types = FALSE)
+# clusters_barplot(tokens, Recipient.medoid)
+# clusters_barplot(tokens, Theme.medoid)
+# clusters_barplot(tokens, tree_cluster)
+# varimp_bin <- read_tsv(file.path(processed_data, "varimp_bin.tsv"), show_col_types = FALSE)
+# varimp_dotplot(varimp_bin)
+# add a freq plot of the distribution of LEMMAS
--- BKL_22/R/read.R
+++ BKL_22/R/read.R
@@ -1,11 +1,7 @@
 library(tidyverse)
 library(semcloud)
 library(Rtsne)
-library(ggrepel)
 library(ggparty)
-library(showtext) # for a fun font
-font_add_google("Schoolbell", "bell")
-showtext_auto()
 
 data_folder <- here::here("BKL_22", "data")
 model_name <- list.dirs(data_folder, recursive = FALSE, full.names = FALSE)
@@ -41,16 +37,18 @@ tree_df <- as_tibble(ggparty(tree_data$tree)$data) %>%
   select(id, nodesize, starts_with("nodedata")) %>%
   unnest(starts_with("nodedata")) %>%
   select(-nodedata_fitted_nodes) %>% 
-  distinct()
+  unite(cluster_combo, nodedata_Recipient.cluster, nodedata_Theme.cluster, sep = "-") %>% 
+  select(cluster_combo, id) %>% 
+  deframe()
 
 ### Tokens with clustering ----
 recipient_clusters <- read_tsv(fnames$clusters_r, show_col_types = FALSE)
 theme_clusters <- read_tsv(fnames$clusters_t, show_col_types = FALSE) 
 tokens <- read_tsv(fnames$tokens, show_col_types = FALSE) %>% 
-  filter(Theme.inlist, Recipient.inlist) %>% 
+  filter(Theme.inlist, Recipient.inlist, Speaker.sex %in% c("F", "M")) %>% 
   select(Token.ID, Context, Variant = Response.variable,
          ends_with("lemma"), ends_with("type")) %>% 
-  mutate(Variant = fct_recode(Variant, ditransitive = "D", prepositional = "P")) %>%
+  mutate(Variant = fct_recode(Variant, prepositional = "P", ditransitive = "D")) %>%
   left_join(
     rename_with(recipient_clusters, ~ paste0("Recipient.", .x)),
     by = c("Recipient.lemma" = "Recipient.type_word")
@@ -63,11 +61,8 @@ tokens <- read_tsv(fnames$tokens, show_col_types = FALSE) %>%
     Recipient.medoid = if_else(Recipient.silhouette < 0, "other", Recipient.medoid_label),
     Theme.medoid = if_else(Theme.silhouette < 0, "other", Theme.medoid_label)
     ) %>% 
-  left_join(
-    select(tree_df, Recipient.medoid_label = nodedata_Recipient.cluster,
-           Theme.medoid_label = nodedata_Theme.cluster, tree_cluster = id),
-    by = c("Theme.medoid_label", "Recipient.medoid_label")) %>% 
-  mutate(tree_cluster = as.factor(tree_cluster))
+  unite(cluster_combo, Recipient.medoid_label, Theme.medoid_label, sep = "-", remove = FALSE) %>% 
+  mutate(tree_cluster = as.factor(tree_df[cluster_combo]))
 
 ### Coordinates ----
 get_coords <- function(filename) {
@@ -90,76 +85,16 @@ theme_coords <- get_coords(fnames$wwmx_t) %>%
   left_join(theme_clusters, by = c("cw" = "type_word")) %>% 
   mutate(final_cluster = if_else(silhouette < 0, NA_character_, medoid_label))
 
-# Plots =========================================================================
 
-## Functions -------------------------------------------------------------------
-tsne_plot <- function(coords, seed = 7) {
-  # Scatterplot with t-sne coordinates, colored by cluster (and negative silhouette
-  # in gray). Instead of a legend the name of the medoid is indicated in its
-  # approximate position (and the same color).
-  ggplot(coords, aes(x = x, y = y, color = final_cluster)) +
-    geom_point(size = 3, alpha = 0.7) +
-    geom_label_repel(data = filter(coords, cw == medoid_label),
-                     aes(label = cw),
-                     seed = seed, size = 8) +
-    theme_void() +
-    scale_color_viridis_d(na.value = "gray", guide = "none") +
-    coord_fixed()
-}
-add_point <- function(p, lemma_data, add_x = 1, add_y = 1, curvature = -0.2,
-                      hjust = 1, vjust = -0.1, font_size = 10) {
-  p + annotate("point", x = lemma_data$x, y = lemma_data$y, shape = 1, size = 5) +
-    annotate("curve",
-             x = lemma_data$x + add_x, xend = lemma_data$x, 
-             y = lemma_data$y + add_y, yend = lemma_data$y,
-             size = 1, arrow = arrow(length = unit(0.15, "inches")),
-             curvature = curvature) +
-    annotate("text",
-             x = lemma_data$x + add_x, y = lemma_data$y + add_y,
-             label = lemma_data$cw, hjust = hjust, vjust = vjust, family = "bell", size = font_size)
-}
+# Write data ===============================================================================
 
-clusters_barplot <- function(column) {
-  # Barplot with the number of tokens covered by each cluster, with different
-  # shades of gray for each variant.
-  ggplot(tokens, aes(x = fct_infreq({{ column }}), fill = Variant)) +
-    geom_bar() +
-    scale_fill_grey(start = 0.4, end = 0.6) +
-    labs(x = "Medoid", y = "N of tokens") +
-    theme_minimal() +
-    theme(legend.position = c(0.9, 0.9)) +
-    coord_flip()
-}
-varimp_dotplot <- function(varimp_data) {
-  enframe(varimp_data, name = "Predictor", value = "Varimp") %>% 
-    mutate(
-      Predictor = str_remove(Predictor, ".bin"),
-      Slot = str_extract(Predictor, "^[RT][a-z]+")) %>% 
-    ggplot(aes(x = Varimp, y = reorder(Predictor, Varimp),
-               color = Slot)) +
-    geom_point(size = 2) +
-    scale_color_viridis_d(begin = 0.2, end = 0.8, option = "A") +
-    labs(x = "Variable importance", y = "Predictor") +
-    geom_vline(xintercept = abs(min(varimp_bin)),
-               color = "gray30", linetype = 2) +
-    theme_minimal(base_size = 20) +
-    theme(legend.position = c(0.9, 0.2))
-}
-
-## Actual plotting -------------------------------------------------------------
-# tsne_plot(recipient_coords) %>%
-#   add_point(recipient_coords %>% filter(cw == "government/nn"), -6, 0) %>%
-#   add_point(recipient_coords %>% filter(cw == "mother/nn"), -7)
-# 
-# tsne_plot(theme_coords) %>%
-#   add_point(theme_coords %>% filter(cw == "it/pp"), -7, -10, vjust = 1)
-# 
-# clusters_barplot(Recipient.medoid)
-# clusters_barplot(Theme.medoid)
-# clusters_barplot(tree_cluster)
-# varimp_dotplot(varimp_bin)
-# add a freq plot of the distribution of LEMMAS
-
+output_dir <- here::here("BKL_22", "data_processed")
+output_files <- file.path(output_dir, paste0(c("dat_clusters", "recipient_coords", "theme_coords", "varimp_bin"),".tsv"))
+walk2(
+  list(tokens, recipient_coords, theme_coords, enframe(varimp_bin, name = "Predictor", value = "Varimp")),
+  output_files,
+  write_tsv
+)
 # Experiment ----
 # check the main clusters in tree results
 if (FALSE) {
